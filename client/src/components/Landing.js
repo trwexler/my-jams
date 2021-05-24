fab424bc484bf8a9a7610b885ddb13943c17e5ee
Rob <rwr217@gmail.com>
2021-05-24 12:11:55 -0500

    updated carousel timing and slides shown

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -54,12 +54,15 @@ const Landing = (props) => {
     initialSlide: 0,
     autoplay: true,
     draggable: true,
+    swipe: true,
+    swipeToSlide: true,
+
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -67,8 +70,8 @@ const Landing = (props) => {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 3,
+          slidesToScroll: 1,
           initialSlide: 2,
         },
       },
