e60e692fffd0a5641daefa957ed6871afde324b6
Rob <rwr217@gmail.com>
2021-05-24 12:03:12 -0500

    Carousel remade

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,151 +1,127 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import {Link, navigate, Router} from '@reach/router';
-import LandingCarousel from './LandingCarousel';
-import AddButton from './AddButton';
-import Header from './Header';
-import { Carousel } from 'react-bootstrap';
-import Slider from 'react-slick';
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, navigate, Router } from "@reach/router";
+import LandingCarousel from "./LandingCarousel";
+import AddButton from "./AddButton";
+import Header from "./Header";
+import { Carousel } from "react-bootstrap";
+import Slider from "react-slick";
 
 // 523532 API KEY
-const Landing = (props)=>{
+const Landing = (props) => {
+  const [artistList, setArtistList] = useState([]);
+  const { user, setUser } = props;
 
-    const [artistList, setArtistList] = useState([]);
-    const {user, setUser} = props;
-
-//for user method tests
-useEffect(()=>{
+  //for user method tests
+  useEffect(() => {
     setUser({
-        firstName: "joe",
-        lastName: "walker",
-        userName: "joejoe",
-        id: "22222",
-        albums:[],
-        tracks:[],
-        artists: [],
-        email: "joe@aol.com",
-        password: "123456789",
-    })
-},[])
+      firstName: "joe",
+      lastName: "walker",
+      userName: "joejoe",
+      id: "22222",
+      albums: [],
+      tracks: [],
+      artists: [],
+      email: "joe@aol.com",
+      password: "123456789",
+    });
+  }, []);
 
+  useEffect(() => {
+    axios
+      .get(
+        "https://theaudiodb.com/api/v1/json/523532/mostloved.php?format=track"
+      )
+      .then((res) => {
+        console.log("mostloved", res.data);
+        console.log(res.data.loved);
+        setArtistList(res.data.loved);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  // Size = number of artists displayed on landing page.
+  let size = 15;
 
-    useEffect(()=>{
-        axios.get('https://theaudiodb.com/api/v1/json/523532/mostloved.php?format=track')
-            .then((res)=>{
-                console.log('mostloved', res.data);
-                console.log(res.data.loved);
-                setArtistList(res.data.loved);
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: true,
+    draggable: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-    },[])
+  return (
+    <div>
+      <Header user={user} />
+      <h1>Landing</h1>
 
-    // Size = number of artists displayed on landing page.
-    let size=15;
-
-    let settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 4,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-        },]}
-
-    return(
+      {artistList ? (
         <div>
-            <Header user={user}/>
-            <h1>Landing</h1>
-            
-            {
-                artistList?
-                <div>
-                
-                    <div>
-                    <div class="container-fluid">
-                        <div className="row title" style={{ marginBottom: "20px" }}>
-                            <div class="col-sm-12">
-                                Most Loved Artists
-                            </div>
-                        </div>
-                </div>
-                <div className="container-fluid">
-                    <Carousel>
-                    {
-                artistList.slice(0, size).map((artist,index)=>(
-                        <Carousel.Item style={{ height: "300px" }}>
-                                <img
-                                style={{ height: "300px" }}
-                                className="d-block w-100"
-                                src={artist.strTrackThumb}
-                                />
-                            <Carousel.Caption>
-                            <Link to={`/artist/${artist.idArtist}/${user.id}`}><h3>{artist.strArtist}</h3></Link>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                         ))
-                        }
-                </Carousel>
+          <div>
+            <div class="container-fluid">
+              <div className="row title" style={{ marginBottom: "20px" }}>
+                <div class="col-sm-12">Most Loved Artists</div>
+              </div>
             </div>
+            <div>
+              <Slider {...settings}>
+                {artistList.slice(0, size).map((artist, index) => (
+                  <div>
+                    <Link to={`/artist/${artist.idArtist}/${user.id}`}>
+                      <img src={artist.strTrackThumb} />
+                    </Link>
+                    <Link to={`/artist/${artist.idArtist}/${user.id}`}>
+                      <h3>{artist.strArtist}</h3>
+                    </Link>
+                  </div>
+                ))}
+              </Slider>
             </div>
-               
-                </div>
-                : <h1>Loading...</h1>
-            }
+          </div>
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
 
-            {/* {
+      {/* {
                 artistList.map((artist,index)=>(
                     <Link to={`/artist/${artist.idArtist}/${user.id}`}><p key={index}>{artist.strArtist}</p></Link>
                 ))
             } */}
-
-<Slider {...settings}
-        //   slidesToShow={5}
-        //   swipeToSlide={true}
-        //   focusOnSelect={true}
-        >
-            {
-                artistList.slice(0, size).map((artist,index)=>(
-          <div>
-            <Link to={`/artist/${artist.idArtist}/${user.id}`}><img src={artist.strTrackThumb}/></Link>
-            <Link to={`/artist/${artist.idArtist}/${user.id}`}><h3>{artist.strArtist}</h3></Link>
-          </div>
-                ))}
-        </Slider>
- 
-        </div>
-    )
-
-}
-
+    </div>
+  );
+};
 
 export default Landing;
